78ed711351e3bdad98db7a27f78594eaf1732f65
felix malsch <malsch.felix@web.de>
2022-01-03 19:58:35 +0100

    changes to collectPotentialNextPoints to avoid problems at corners and make it easier to work with in colorInjection

--- connectionDetector.py
+++ connectionDetector.py
@@ -17,12 +17,13 @@ def DEBUG_VISUAL(slopes, img, r, current_point, last_slope):
         cv2.circle(new_img, tuple(current_point + last_slope), 3, (255,100,90), thickness = -1)
         
         cv2.imshow('Connection Detection', new_img)
-        if cv2.waitKey(50) != -1:
+        if cv2.waitKey(5000) != -1:
             if cv2.waitKey(200) != -1: return 'break'
         ###########end   visualization for debugging###############
 
   
 
+inf = 1000000
   
 def isEndpoint(p):
     return False
@@ -31,39 +32,30 @@ def isEndpoint(p):
 
 def isCloseToBlack(c):
     
-    threshold = 15
+    threshold = 0.5
     color_magnitude = c #math.sqrt(c[0] ** 2 + c[1] ** 2 + c[2] ** 2)
     return color_magnitude < threshold
-  
-  
 
-def closestSlope(slope, slopes):
+
+  
+def getPerimeterBorder(r):
+    perimeter_left   = [np.array([0        , 2*r-1 - i]) for i in range(2*r)]
+    perimeter_top    = [np.array([i        , 0        ]) for i in range(2*r)]
+    perimeter_right  = [np.array([2*r-1    , i        ]) for i in range(2*r)]    
+    perimeter_bottom = [np.array([2*r-1 - i, 2*r-1    ]) for i in range(2*r)]
     
-    slope_magnitude = np.linalg.norm(slope)
-    normalized_slope = slope / slope_magnitude
+    return perimeter_left + perimeter_top + perimeter_right + perimeter_bottom
+
+
+
+def collectPotentialNextPoints(img, r, x, y):
     
-    max_dot_product = -1
-    best_slope = np.zeros(shape=(2,), dtype=np.int32)
+    perimeter_border = getPerimeterBorder(r)
     
-    for s in slopes:
-        
-        dot_product = sum(normalized_slope * s / np.linalg.norm(s))
-        
-        if (dot_product > max_dot_product):
-            
-            max_dot_product = dot_product
-            best_slope = s
-        
-    return best_slope
-        
-        
-    
-def collectIntersections(perimeter_border):
-   
     intersections = []
     
     isOnIntersection = False
-    avg_coord = 0
+    avg_index = 0
     i = 0
     count = 0
         
@@ -71,10 +63,10 @@ def collectIntersections(perimeter_border):
             
         i += 1
             
-        if(isCloseToBlack(p)):
+        if isCloseToBlack(img[y - r + p[1], x - r + p[0]]):
                
             isOnIntersection = True
-            avg_coord += i
+            avg_index += i
             count += 1
                 
         else:
@@ -82,48 +74,36 @@ def collectIntersections(perimeter_border):
             if isOnIntersection:
                     
                 isOnIntersection = False
-                avg_coord /= count
-                avg_coord -= len(perimeter_border) / 2
-                intersections.append(avg_coord)
-                avg_coord = 0
+                avg_index = int(avg_index / count)
+                intersections.append(perimeter_border[avg_index] - np.array([r, r]))
+                avg_index = 0
                 count = 0
                 
-    if isOnIntersection:    #if the side ends on an intersection, we still need to register it
-        avg_coord /= count
-        avg_coord -= len(perimeter_border) / 2
-        intersections.append(avg_coord)
-                
     return intersections
-
-
-
-def collectPotentialNextPoints(img, r, x, y):
-    perimeter_left   = img[ y+r : y-r : -1,   x-r            ]
-    perimeter_top    = img[ y-r           ,   x-r : x+r      ]
-    perimeter_right  = img[ y-r : y+r     ,   x+r            ]
-    perimeter_bottom = img[ y+r           ,   x+r : x-r : -1 ]
-        
-    #iterate around perimeter and register intersections
-    intersections_left   = collectIntersections(perimeter_left)
-    intersections_top    = collectIntersections(perimeter_top)
-    intersections_right  = collectIntersections(perimeter_right)
-    intersections_bottom = collectIntersections(perimeter_bottom)
-        
-    #choose most plausible intersection, by slope continuation or color injection   
-    slopes = []
-        
-    for dy in intersections_left   : slopes.append( np.array([  -r, -dy ], dtype=np.int32) )
-    for dx in intersections_top    : slopes.append( np.array([  dx,  -r ], dtype=np.int32) )
-    for dy in intersections_right  : slopes.append( np.array([   r,  dy ], dtype=np.int32) )
-    for dx in intersections_bottom : slopes.append( np.array([ -dx,   r ], dtype=np.int32) )
     
-    return slopes
+    
+
+def indexOfClosestElementInList(e, l):
+    #find last_slope in slopes
+    min_distance = inf
+    index = -1
+    
+    for i in range(len(l)):
+    
+        d = np.linalg.norm(l[i] - e)
+        
+        if d < min_distance:
+            min_distance = d
+            index = i
+            
+    return index
 
 
 
 def processImageForSlopeFollowing(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((3, 3), np.uint8)
+    #double scale
     img = cv2.erode(img, kernel, iterations = 2)
     img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
     #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
@@ -149,51 +129,49 @@ def processImageForColorInjection(img):
 
 
 
+def closestSlope(slope, slopes):
+    
+    slope_magnitude = np.linalg.norm(slope)
+    normalized_slope = slope / slope_magnitude
+    
+    max_dot_product = -1
+    best_slope = np.zeros(shape=(2,), dtype=np.int32)
+    
+    for s in slopes:
+        
+        dot_product = sum(normalized_slope * s / np.linalg.norm(s))
+        
+        if (dot_product > max_dot_product):
+            
+            max_dot_product = dot_product
+            best_slope = s
+        
+    return best_slope
+        
+        
+
 def colorInjection(last_slope, slopes, window):
     
     window = window.copy()
     
-    #find last_slope in slopes
-    min_distance = 100000
-    start_index = -1
+    start_index = indexOfClosestElementInList(last_slope, slopes)
+    r = int(window.shape[0] / 2)
+    perimeter_border = getPerimeterBorder(r)
     
-    for i in range(len(slopes)):
-    
-        d = np.linalg.norm(slopes[i] - last_slope)
-        
-        if d < min_distance:
-            min_distance = d
-            start_index = i
-            
-    #iterate from there through slopes
     injection_points = []
     
+    #inject color 
     for i in range(len(slopes)):
         
         index = (start_index + i) % len(slopes)
         
-        #inject color
-        p = slopes[index]
-        r = int(window.shape[0] / 2)
+        injection_point = slopes[index] + (r, r)
+        border_index = indexOfClosestElementInList(injection_point, perimeter_border)
         
-        step = (-int(p[1] / r), int(p[0] / r))#(int(p[0] / r), int(p[1] / r))
-        
-        if step == (-1, -1):
-            step = (1, 0)
-        elif step == (1, -1):
-            step = (0, 1)
-        elif step == (1, 1):
-            step = (-1, 0)  
-        elif step == (-1, 1):
-            step = (0, -1)
-        
-        injection_point = p + (r, r) + step
-        
-        if(injection_point[0] == 2 * r):
-            injection_point[0] = 2 * r - 1
-        if(injection_point[1] == 2 * r):
-            injection_point[1] = 2 * r - 1
-            
+        while isCloseToBlack(window[injection_point[1], injection_point[0]]):
+            border_index = (border_index + 1) % len(perimeter_border)
+            injection_point = perimeter_border[border_index]
+         
         injection_point = tuple(injection_point)
             
         injection_points.append(injection_point)
@@ -205,21 +183,24 @@ def colorInjection(last_slope, slopes, window):
     
     for p in injection_points:
         
-        color_order.append(window[p[0], p[1]])
-        
+        color_order.append(window[p[1], p[0]])
+    print(color_order)
     #lookup meaning of color order
     if color_order == [1, 4, 3, 4]:
-        return slopes[start_index + 1]
+        return slopes[(start_index + 1)%len(slopes)]
     elif color_order == [1, 2, 3, 4]:
-        return slopes[start_index + 2]
+        return slopes[(start_index + 2)%len(slopes)]
     elif color_order == [3, 2, 3, 4]:
-        return slopes[start_index + 3]
+        return slopes[(start_index + 3)%len(slopes)]
     
-    return -1
+    return None
 
 
    
 def traceConnection(img, x_start, y_start, x_end, y_end, r):
+    
+    img_slope_following = processImageForSlopeFollowing(img)
+    img_color_injection = processImageForColorInjection(img)
     
     current_point = np.array([x_start, y_start], dtype=np.int32)
     #pad image
@@ -231,48 +212,70 @@ def traceConnection(img, x_start, y_start, x_end, y_end, r):
         x = current_point[0]
         y = current_point[1]
         
-        slopes = collectPotentialNextPoints(img, r, x, y)
+        slopes = collectPotentialNextPoints(img_slope_following, r, x, y)
+        last_slope_1 = closestSlope(last_slope, slopes)
         
         
-        
-        last_slope = closestSlope(last_slope, slopes)
+        last_slope_2 = None
+        if len(slopes) == 4:
+            window = img_color_injection[ y-r : y+r, x-r : x+r ]
+            last_slope_2 = colorInjection(np.array([0, 0]) - last_slope, slopes, window)
+            if last_slope_2 is not None:
+                last_slope = last_slope_2
+                
+        if not np.array_equal(last_slope, last_slope_2):
+            last_slope = last_slope_1
         
         if DEBUG_VISUAL(slopes, img, r, current_point, last_slope) == 'break': break
         
         current_point += last_slope   #last_slope is still current slope at this point but already called last_slope for next step
-          
+        
+        
     return current_point
 
+
+
+#########################################
+
+
 def mouseCallback(event, x, y, flags, param):
-    global video, isRunning
+    #global video, isRunning
     
-    ret, img = video.read()
+    #ret, img = video.read()
     #img = processImageForSlopeFollowing(img)
-    img = processImageForColorInjection(img)
+    global img
     if event == cv2.EVENT_LBUTTONDOWN:
-        traceConnection(img, x, y, 135, 50, 6)
+        traceConnection(img, x, y, 135, 50, 10)
         isRunning = True
     
     
-
-video = cv2.VideoCapture(0)
-ret, img = video.read()
-
-fps = video.get(cv2.CAP_PROP_FPS)
-
+############################
+img = cv2.imread('connections_test_2.jpg', cv2.IMREAD_COLOR)
 cv2.namedWindow('Connection Detection')
+cv2.imshow('Connection Detection', img)
 cv2.setMouseCallback('Connection Detection', mouseCallback)
-
-isRunning = False
-
-while video.isOpened():
-    
-    if not isRunning:
-        ret, frame = video.read()
-    
-        cv2.imshow('Connection Detection', frame)
-    
-    if cv2.waitKey(int(1000 / fps) + 1) != -1:
-        break
-
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+############################
+
+# video = cv2.VideoCapture(0)
+# ret, img = video.read()
+
+# fps = video.get(cv2.CAP_PROP_FPS)
+
+# cv2.namedWindow('Connection Detection')
+# cv2.setMouseCallback('Connection Detection', mouseCallback)
+
+# isRunning = False
+
+# while video.isOpened():
+    
+#     if not isRunning:
+#         ret, frame = video.read()
+    
+#         cv2.imshow('Connection Detection', frame)
+    
+#     if cv2.waitKey(int(1000 / fps) + 1) != -1:
+#         break
+
+#cv2.destroyAllWindows()
