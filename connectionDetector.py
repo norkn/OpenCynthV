2edfd164f5b185358c4f3240c8c24341db1db84f
felix malsch <malsch.felix@web.de>
2022-01-13 12:32:44 +0100

    fixed bug in removal of disconnected intersections code cleanup

--- connectionDetector.py
+++ connectionDetector.py
@@ -4,22 +4,24 @@ import cv2
 
 def DEBUG_VISUAL(slopes, img, r, current_point, last_slope):
     ###########begin visualization for debugging###############
-        #print('step ', len(slopes))
-        new_img = img.copy()#cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    global input_img
+    new_img = input_img.copy()#cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)#img.copy()#cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    
+    cv2.rectangle(new_img, tuple(current_point - (r, r)), tuple(current_point + (r, r)), (0, 255, 0))            
+             
+    cv2.circle(new_img, tuple(current_point), 2, (0,0,255), thickness = -1)
+    
+    for s in slopes:
+        cv2.circle(new_img, tuple(current_point + s), 2, (0,0,255), thickness = -1)
         
-        cv2.rectangle(new_img, tuple(current_point - (r, r)), tuple(current_point + (r, r)), (0, 255, 0))            
-                 
-        cv2.circle(new_img, tuple(current_point), 2, (0,0,255), thickness = -1)
-        
-        for s in slopes:
-            cv2.circle(new_img, tuple(current_point + s), 2, (0,0,255), thickness = -1)
-            
-        cv2.circle(new_img, tuple(current_point + last_slope), 3, (255,100,90), thickness = -1)
-        
-        cv2.imshow('Connection Detection', new_img)
-        if cv2.waitKey(5000) != -1:
-            if cv2.waitKey(200) != -1: return 'break'
-        ###########end   visualization for debugging###############
+    cv2.circle(new_img, tuple(current_point + last_slope), 3, (255,100,90), thickness = -1)
+    
+    cv2.imshow('Connection Detection', new_img)
+    if cv2.waitKey(50) != -1:
+        if cv2.waitKey(20) != -1:
+            cv2.destroyAllWindows()
+            quit()
+    ###########end   visualization for debugging###############
 
   
 
@@ -84,7 +86,7 @@ def collectPotentialNextPoints(img, r, x, y):
     
 
 def indexOfClosestElementInList(e, l):
-    #find last_slope in slopes
+    
     min_distance = inf
     index = -1
     
@@ -100,79 +102,17 @@ def indexOfClosestElementInList(e, l):
 
 
 
-def processImageForSlopeFollowing(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((3, 3), np.uint8)
-    #double scale
-    img = cv2.erode(img, kernel, iterations = 2)
-    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-    #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    img = cv2.erode(img, kernel, iterations = 2)
-    img = cv2.dilate(img, kernel, iterations = 2)
-    #img = cv2.medianBlur(img, 9)
-    img = cv2.dilate(img, kernel, iterations = 1)
-    
-    return img
-
-
-
-def processImageForColorInjection2(img):
+def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    # for y in range(img.shape[0]):
-    #     for x in range(img.shape[1]):
-    #             img[y,x] = np.clip(1.5 * img[y,x], 0, 255)
-    #cv2.convertScaleAbs(img, alpha=1.5, beta=0)
-    
     kernel = np.ones((3, 3), np.uint8)
-    # kernel[0, 0] = 0
-    # kernel[2, 0] = 0
-    # kernel[2, 2] = 0
-    # kernel[0, 2] = 0
-    img = cv2.erode(img, kernel, iterations = 2)
-    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-    #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    
-    # img = cv2.dilate(img, kernel, iterations = 1)
-    # img = cv2.erode(img, kernel, iterations = 1)
-    
-    img = cv2.erode(img, kernel, iterations = 2)
-    img = cv2.dilate(img, kernel, iterations = 2)
-    img = cv2.medianBlur(img, 5)
-    img = cv2.dilate(img, kernel, iterations = 1)
-    
-    #how about: piecewise: color inject count colors c; while(new colors=c){erode;color inject and count new colors;}
-    
-    return img
 
-def processImageForColorInjection(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # for y in range(img.shape[0]):
-    #     for x in range(img.shape[1]):
-    #             img[y,x] = np.clip(1.5 * img[y,x], 0, 255)
-    #cv2.convertScaleAbs(img, alpha=1.5, beta=0)
-    
-    kernel = np.ones((3, 3), np.uint8)
-    # kernel[0, 0] = 0
-    # kernel[2, 0] = 0
-    # kernel[2, 2] = 0
-    # kernel[0, 2] = 0
     img = cv2.GaussianBlur(img,(3,3),8)
-    img = cv2.erode(img, kernel, iterations = 1)
-    
-    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,4)
-    #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,3)
-    
-    # img = cv2.dilate(img, kernel, iterations = 1)
-    # img = cv2.erode(img, kernel, iterations = 1)
-    
+    img = cv2.erode(img, kernel, iterations = 1)   
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,4)    
     img = cv2.erode(img, kernel, iterations = 2)
-    #img = cv2.dilate(img, kernel, iterations = 2)
     img = cv2.medianBlur(img, 5)
     img = cv2.dilate(img, kernel, iterations = 2)
-    
-    #how about: piecewise: color inject count colors c; while(new colors=c){erode;color inject and count new colors;}
     
     return img
 
@@ -233,7 +173,6 @@ def colorInjection(last_slope, slopes, window):
     for p in injection_points:
         
         color_order.append(window[p[1], p[0]])
-    print(color_order)
     
     #remove unconnected intersections
     i = 0
@@ -244,9 +183,10 @@ def colorInjection(last_slope, slopes, window):
             
             color_order.pop(i + 1)
             
-            slopes.pop((start_index + i + 1) % len(slopes))
-            if start_index > i + 1: start_index -= 1
-            
+            index_of_disconnected_point = (start_index + i + 1) % len(slopes)
+            slopes.pop(index_of_disconnected_point)
+            if start_index > index_of_disconnected_point: start_index -= 1
+            print(start_index)
             for j in range(i, len(color_order)):
                                
                 color_order[j] -= 1
@@ -262,7 +202,7 @@ def colorInjection(last_slope, slopes, window):
                            (1, 2, 3, 4) : 2,
                            (3, 2, 3, 4) : 3,
                            (1, 5, 3, 4, 5) : 1, #fused point on perimeter
-                           (1, 2, 3, 4, 5) : 2, #ambiguous case w\ flip
+                           #(1, 2, 3, 4, 5) : 2, #ambiguous case w\ flip
                            (4, 2, 3, 4, 5) : 4, #flipped case fuse point on perimeter
                            #(1, 2, 3, 4, 5) : 3 #ambiguous case w\ flip
                            (1, 6, 3, 6, 5, 6) : 1,
@@ -272,7 +212,7 @@ def colorInjection(last_slope, slopes, window):
                            #(1, 2, 3, 4, 5, 6) : 2, #ambiguous case, gets overwritten
                            (1, 2, 5, 4, 5, 6) : 2,
                            (3, 2, 3, 6, 5, 6) : 3,
-                           (1, 2, 3, 4, 5, 6) : 3, #ambiguous case, might also sometimes mean 2. more often 3 though
+                           #(1, 2, 3, 4, 5, 6) : 3, #ambiguous case, might also sometimes mean 2. more often 3 though
                            #symmetric cases for 6 intersections
                            (5, 2, 5, 4, 5, 6) : 5,
                            (5, 2, 3, 4, 5, 6) : 5,
@@ -288,14 +228,77 @@ def colorInjection(last_slope, slopes, window):
         point_number = color_order_meaning[color_order]
         return True, slopes[(start_index + point_number)%len(slopes)]
     
+    if len(color_order) == 2:
+        return True, slopes[(start_index + 1)%len(slopes)]
+    
     return False, slopes
 
 
-   
+
+def chooseNextPoint(img, r, current_point, last_slope):
+    
+    x = current_point[0]
+    y = current_point[1]
+    
+    window = img[ y-r : y+r, x-r : x+r ]
+    next_points = collectPotentialNextPoints(window, r, r, r)
+    
+    ret, col_inj_result = colorInjection(np.array([0, 0]) - last_slope, next_points, window)
+    
+    if ret:
+        last_slope = col_inj_result
+        DEBUG_VISUAL(next_points, img, r, current_point, last_slope)
+        current_point += last_slope
+    else:
+        DEBUG_points = next_points
+        next_points = collectPotentialNextPoints(img, r, x, y)
+        last_slope = closestSlope(last_slope, col_inj_result)#next_points)
+        DEBUG_VISUAL(DEBUG_points, img, r, current_point, last_slope)
+        current_point += last_slope
+    
+    return current_point, last_slope
+
+
+
+def countVotes(l):
+    
+    dictionary = {}
+    
+    for element in l:
+        
+        element = tuple(element)
+        
+        if element in dictionary: 
+            dictionary[element] += 1
+        else:      
+            dictionary[element] = 1
+      
+    result = None
+    max_votes = -1
+    
+    for element in dictionary.keys():
+        
+        if dictionary[element] > max_votes:
+            result = element
+            max_votes = dictionary[element]
+            
+    return result
+
+
+
+class Attempt:
+    r = 0
+    
+    def __init__(self, _r):
+            self.r = _r
+         
+ 
+           
 def traceConnection(img, x_start, y_start, x_end, y_end, r):
     
-    img_slope_following = processImageForSlopeFollowing(img)
-    img_color_injection = processImageForColorInjection(img)
+    attempts = [Attempt(r)]#Attempt(r+2), Attempt(r), Attempt(r-2)]
+    
+    img_color_injection = preprocess(img)
     
     current_point = np.array([x_start, y_start], dtype=np.int32)
     #pad image
@@ -303,36 +306,16 @@ def traceConnection(img, x_start, y_start, x_end, y_end, r):
     
     while(not isEndpoint(current_point)):
     
-        #look at pixels in circle or square of certain radius around current point 
-        x = current_point[0]
-        y = current_point[1]
+        current_point_votes = []
+        last_slope_votes = []
         
-        window = img_color_injection[ y-r : y+r, x-r : x+r ]
-        next_points = collectPotentialNextPoints(window, r, r, r)
+        for att in attempts:
+            #look at pixels in circle or square of certain radius around current point 
+            c, l = chooseNextPoint(img_color_injection, att.r, current_point, last_slope)
+            current_point_votes.append(c)
+            last_slope_votes.append(l)
         
-        ret, col_inj_result = colorInjection(np.array([0, 0]) - last_slope, next_points, window)
-        
-        if ret:
-            last_slope = col_inj_result
-        else:
-            next_points = collectPotentialNextPoints(img_color_injection, int(r / 2), x, y)
-            last_slope = closestSlope(last_slope, next_points)
-            
-            #if DEBUG_VISUAL(next_points, img_color_injection, int(r / 2), current_point, last_slope) == 'break': break
-            
-            x = current_point[0]
-            y = current_point[1]
-        
-            next_points = collectPotentialNextPoints(img_color_injection, int(r / 2), x, y)
-            last_slope = closestSlope(last_slope, next_points)
-            
-            #if DEBUG_VISUAL(next_points, img_color_injection, int(r / 2), current_point, last_slope) == 'break': break
-            #last_slope = closestSlope(last_slope, col_inj_result)
-        
-        if DEBUG_VISUAL(next_points, img_color_injection, r, current_point, last_slope) == 'break': break
-        
-        current_point += last_slope   #last_slope is still current slope at this point but already called last_slope for next step
-        
+        current_point, last_slope = np.array(countVotes(current_point_votes)), np.array(countVotes(last_slope_votes))        
         
     return current_point
 
@@ -342,20 +325,17 @@ def traceConnection(img, x_start, y_start, x_end, y_end, r):
 
 
 def mouseCallback(event, x, y, flags, param):
-    #global video, isRunning
-    
-    #ret, img = video.read()
-    #img = processImageForSlopeFollowing(img)
-    global img
+
+    global input_img
     if event == cv2.EVENT_LBUTTONDOWN:
-        traceConnection(img, x, y, 135, 50, 10)
+        traceConnection(input_img, x, y, 135, 50, 26)
         isRunning = True
     
     
 ############################
-img = cv2.imread('connections_test_2.jpg', cv2.IMREAD_COLOR)
+input_img = cv2.imread('connections_test_2.jpg', cv2.IMREAD_COLOR)
 cv2.namedWindow('Connection Detection')
-cv2.imshow('Connection Detection', img)
+cv2.imshow('Connection Detection', input_img)
 cv2.setMouseCallback('Connection Detection', mouseCallback)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
