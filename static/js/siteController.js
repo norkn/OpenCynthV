1fa8575025f0d79418fdbcd414163d398ddb6d8a
Sven M <sven.Maderer@haw-hamburg.de>
2022-01-25 15:00:35 +0100

    Fix

---
+++ static/js/siteController.js
@@ -0,0 +1,4 @@
+function button(){
+    console.log("script läuft");
+    document.getElementById('test').innerHTML = "script ändert den text";
+}
--- static/siteController.js
+++
@@ -1,2 +0,0 @@
-    console.log("script läuft");
-    document.getElementById('test').innerHTML = "script ändert den text"
